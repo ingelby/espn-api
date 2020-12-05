eeecfd2ff2e6a17beecb1314592f89b0047758a1
George <george@ingelby.com>
2020-12-05 17:00:57 +0000

    - Added new league slugs     - Added league slug friendly name mapper

--- src/Constants/LeagueSlug.php
+++ src/Constants/LeagueSlug.php
@@ -9,4 +9,22 @@ class LeagueSlug
     public const NHL = 'nhl';
     public const MLB = 'mbl';
     public const COLLEGE_FOOTBALL = 'college-football';
+    public const UEFA_CHAMPIONS_LEAGUE = 'uefa.champions';
+    public const ENGLISH_PREMIER_LEAGUE = 'eng.1';
+
+    /**
+     * @param string $slug
+     * @return string
+     */
+    public static function mapFriendlyName(string $slug): string
+    {
+        switch ($slug) {
+            case static::UEFA_CHAMPIONS_LEAGUE:
+                return 'UEFA Champions League';
+            case static::ENGLISH_PREMIER_LEAGUE:
+                return 'English Premier League';
+            default:
+                return $slug;
+        }
+    }
 }
